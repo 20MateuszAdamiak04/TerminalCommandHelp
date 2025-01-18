cb5895a2b51bc8892f92b9719ad44b30aa900993
Mateusz Adamiak <MateuszAdamiakAustria@gmail.com>
2025-01-18 22:19:55 +0100

    First Version which works only one.

--- Terminal-Command-Search/Program.cs
+++ Terminal-Command-Search/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        // Wczytanie danych z pliku JSON
+        var json = File.ReadAllText("/Users/mateuszadamiak/Documents/IT-Lernen/Programmierung /C#/Terminal-Command-Search/Terminal-Command-Search/JSON-Files/Test.json");
+        var commands = JsonConvert.DeserializeObject<List<Command>>(json);
+
+        Console.WriteLine("Wpisz frazę, aby znaleźć komendę:");
+        string query = Console.ReadLine();
+
+        // Wyszukiwanie w bazie komend
+        var results = commands.Where(c => c.description.Contains(query, StringComparison.OrdinalIgnoreCase) 
+                                          || c.command.Contains(query, StringComparison.OrdinalIgnoreCase));
+
+        Console.WriteLine("Znalezione komendy:");
+        foreach (var result in results)
+        {
+            Console.WriteLine($"Komenda: {result.command}");
+            Console.WriteLine($"Opis: {result.description}");
+            Console.WriteLine($"Przykład: {result.example}");
+            Console.WriteLine();
+        }
+    }
+}
+
+class Command
+{
+    public string command { get; set; }
+    public string description { get; set; }
+    public string example { get; }
+}    
